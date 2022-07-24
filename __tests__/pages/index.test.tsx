fbd23b465ab1b56b60defd1ccdacc325ad2fb2d1
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2022-07-24 15:52:38 +0700

    edit readme

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -3,7 +3,7 @@ import { mock_donation_list } from '../../constants/mock-data'
 import Home from '../../pages/index'
 
 describe('Home', () => {
-  test('Should render donation list', async () => {
+  test('Should render donation list', () => {
     const { getAllByTestId, getByTestId } = render(<Home data={mock_donation_list}/>)
 
     expect(getAllByTestId('donation-card').length).toEqual(mock_donation_list.length)
@@ -12,12 +12,12 @@ describe('Home', () => {
 
     //test sort by days left
     expect(sortHandlerBtn.textContent).toEqual('Sort by days left')
-    await fireEvent.click(sortHandlerBtn)
+    fireEvent.click(sortHandlerBtn)
     expect(within(getAllByTestId('donation-card')[0]).getByText('#BisaBangkit Bersama Kitabisa'))
 
     // test sort by donation goal
     expect(sortHandlerBtn.textContent).toEqual('Sort by donation goal')
-    await fireEvent.click(sortHandlerBtn)
+    fireEvent.click(sortHandlerBtn)
     expect(within(getAllByTestId('donation-card')[0]).getByText('Bantu Korban Kebakaran Palembang'))
   })
 })
