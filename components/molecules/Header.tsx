156f5ce11474cba87b0908e205470ea6616fea00
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2022-07-24 10:27:56 +0700

    handle sorting

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -1,14 +1,18 @@
-import { NextComponentType } from 'next';
 import Logo from '../atoms/Logo';
 
-const Header:NextComponentType = () => {
+type HeaderProps = {
+  sortBy: string
+  handleSort: () => void
+}
+const Header:React.FC<HeaderProps> = ({ sortBy, handleSort }) => {
   return (
     <div className='flex justify-between p-2'>
       <Logo />
       <button 
         className='rounded-md border border-sky-500 m-2 p-2 bg-sky-600 hover:bg-sky-700 text-white'
+        onClick={handleSort}
       >
-        Sorting by donation goal
+        Sort by { sortBy }
       </button>
     </div>
   )
